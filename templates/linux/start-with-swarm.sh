ec674373a72deac1d9ca4f9acab893b339e4547f
mathieu <dev@lescaudron.com>
2017-08-15 18:16:48 +0200

    Rm service

--- templates/linux/start-with-swarm.sh
+++ templates/linux/start-with-swarm.sh
@@ -10,21 +10,17 @@ docker pull nelioteam/meteord:base
 isServiceExist=$(echo $(docker service ls -f name="$APPNAME") | grep -c "$APPNAME")
 set -e
 
-if  [ "$isServiceExist" == "1" ]; then
-    echo "Update service $APPNAME"
-    docker service update $APPNAME --force --detach=false
-else
-    echo "Create service $APPNAME"
-    docker service create \
-      --replicas 3 \
-      --constraint 'node.role == manager' \
-      --name $APPNAME \
-      --update-delay 10s \
-      --hostname "$HOSTNAME-$APPNAME" \
-      --env-file=$ENV_FILE \
-      --network=nelio_database \
-      --network=meteor_nelio_app \
-      --detach=false \
-      --mount type=bind,source=$BUNDLE_PATH,destination=/bundle \
-      nelioteam/meteord:base
-fi
+docker service rm $APPNAME
+echo "Create service $APPNAME"
+docker service create \
+  --replicas 3 \
+  --constraint 'node.role == manager' \
+  --name $APPNAME \
+  --update-delay 10s \
+  --hostname "$HOSTNAME-$APPNAME" \
+  --env-file=$ENV_FILE \
+  --network=nelio_database \
+  --network=meteor_nelio_app \
+  --detach=false \
+  --mount type=bind,source=$BUNDLE_PATH,destination=/bundle \
+  nelioteam/meteord:base
